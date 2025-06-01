ef39a0a28ffadb1c7eb6e71a73bfd2da9dfc5bbe
Edrian <razer4832@gmail.com>
2025-06-01 05:47:52 +0300

    i changed the functions : check if alpha , and check process state -in check if alpha i add .strip().capatalize() to user in input in order that input will be more flexible -in check process state: i replaced the for in range to for state in process state, and by that if changed to if input_process = state.name

--- library_functions_good.py
+++ library_functions_good.py
@@ -20,7 +20,8 @@ def try_input(prompt: str) -> str | None :
 def check_if_alpha(user_input: str) -> str:
     while not user_input.isalpha():
         print("this input  isn't a string try to type again please")
-        user_input = input("please enter again and please enter a correct string: ")
+        user_input = try_input("please enter again and please enter a correct string: ").strip().capitalize()
+        print(user_input)
     return str(user_input)
 
 
@@ -31,16 +32,17 @@ def is_in_dictionary(key, dictionary) -> int:
     return key
 
 
-def check_process_states(input) -> str | None:
+def check_process_states(input_process) -> str | None:
     while True:
-        for i in range (1, 6, 1): 
-            if input == Process_State(i).name:
+        print(input_process)
+        for state in Process_State:
+            if input_process == state.name:
                 print("the process state is ok")
-                return str(input)
+                return str(input_process)
         else:
-                print("invalid process state please try again")
-                input = check_if_alpha(try_input("please enter a process state: "))
-        return
+            print("invalid process state please try again")
+            input_process = check_if_alpha(try_input("please enter a process state: ").strip().capitalize())
+        
 
 
 def input_files() -> list[str]:
