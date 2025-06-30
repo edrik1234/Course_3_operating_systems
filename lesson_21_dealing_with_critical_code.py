a0bc844d0029ecdc04c4a75a090295729c3b4394
edrik1234 <edrik4832@gmail.com>
2025-06-30 21:07:42 +0300

    Update lesson_21_dealing_with_critical_code.py
    for nadav changes

--- lesson_21_dealing_with_critical_code.py
+++ lesson_21_dealing_with_critical_code.py
@@ -7,24 +7,22 @@ COUNTERS = [0] * NUMBER_OF_THREADS
 def worker(index):
     global COUNTERS
     for i in range(10000):
-      #  with mutex:
             temp = COUNTERS[index]
             temp += 1
             time.sleep(0.0001)
             COUNTERS[index] = temp
 
 def main():
-   # mutex = threading.Lock()
     threads = []
     for i in range(NUMBER_OF_THREADS):
         thread = threading.Thread(target = worker, args = (i))
         threads.append(thread)
         thread.start()
+
+
+    for thread in threads:
         thread.join()# you tell 1 worker do a parrallel dig instead of three workers at the same time
 
-   # for thread in threads:
-  #      thread.join()
-    
     counter = sum(COUNTERS)
     print(counter)
 
