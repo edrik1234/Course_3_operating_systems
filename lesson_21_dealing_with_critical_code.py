8c2c6df17950e584be5efeb8be9c102642ea1f4a
edrik1234 <edrik4832@gmail.com>
2025-07-02 12:15:35 +0300

    Update lesson_21_dealing_with_critical_code.py
    we changes identations and spaces , by making two line spaces between start variables and functions , between functions itself , and between imports and start variables

--- lesson_21_dealing_with_critical_code.py
+++ lesson_21_dealing_with_critical_code.py
@@ -1,16 +1,20 @@
 import threading
 import time
 
+
 NUMBER_OF_THREADS = 3
 COUNTERS = [0] * NUMBER_OF_THREADS
 
+
 def worker(index):
-global COUNTERS
-for i in range(10000):
-    temp = COUNTERS[index]
-    temp += 1
-    time.sleep(0.0001)
-    COUNTERS[index] = temp
+    global COUNTERS
+    for i in range(10000):
+        temp = COUNTERS[index]
+        temp += 1
+        time.sleep(0.0001)
+        COUNTERS[index] = temp
+
+
 def main():
     threads = []
     for i in range(NUMBER_OF_THREADS):
@@ -21,6 +25,7 @@ def main():
         thread.join()# you tell 1 worker do a parrallel dig instead of three workers at the same time
     counter = sum(COUNTERS)
     print(counter)
+
 
 if __name__ == "__main__":
     main()
