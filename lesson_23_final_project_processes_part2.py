575d591457927e5487c40db16516ef14c69f8d63
edrik1234 <edrik4832@gmail.com>
2025-07-02 06:34:12 +0300

    Update lesson_23_final_project_processes_part2.py
    we get out the double lock , and deleted one if to combine two if to one if

--- lesson_23_final_project_processes_part2.py
+++ lesson_23_final_project_processes_part2.py
@@ -14,19 +14,11 @@ def downloader(url, process_index, queue, shared_total, shared_data, lock = None
         with lock:
             time.sleep(0.001)
             shared_total.value += char_count
-
-   
-    if lock:
-        with lock:
             shared_data[url] = {
                 'chars': char_count,
                 'source': process_index
             }
-
-
-    queue.put((process_index, url, char_count))
-
-
+queue.put((process_index, url, char_count))
 def main(lock = True):
     queue = multiprocessing.Queue()
     lock = multiprocessing.Lock() if lock else None
